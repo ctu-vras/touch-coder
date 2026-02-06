8fe7837288d1fc1f79262b33290d4ff17089a870
navarlu <lukannavara@gmail.com>
2026-02-06 09:05:52 +0100

    Use setup-python in legacy container

--- src/video_model.py
+++ src/video_model.py
@@ -65,11 +65,11 @@ class Video:
         
 
         if sys.platform.startswith("win"):
-            self.program_version = "7.5.5 (Windows)"
+            self.program_version = "7.5.6 (Windows)"
         elif sys.platform.startswith("linux"):
-            self.program_version = "7.5.5 (Linux)"
+            self.program_version = "7.5.6 (Linux)"
         else:
-            self.program_version = "7.5.5 (Unknown OS)"
+            self.program_version = "7.5.6 (Unknown OS)"
         print("INFO: Program version:", self.program_version)
         self.parameter1_name = None
         self.parameter2_name = None
